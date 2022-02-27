37d58d0734c7422f6a611c9074248372eae8ace5
Jakub Bateľ <jakub.batel.jb@gmail.com>
2022-02-27 11:23:08 +0100

    G03 - Unused variable

---
+++ app/src/main/java/cz/muni/fi/jana/analyzer/impl/UnusedVariablesAnalyzer.java
@@ -0,0 +1,36 @@
+package cz.muni.fi.jana.analyzer.impl;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.expr.VariableDeclarationExpr;
+import com.github.javaparser.ast.stmt.ExpressionStmt;
+import cz.muni.fi.jana.analyzer.Analyzer;
+import cz.muni.fi.jana.analyzer.issues.IssuesCodes;
+import cz.muni.fi.jana.analyzer.issues.RawIssue;
+
+public class UnusedVariablesAnalyzer extends Analyzer {
+
+    @Override
+    public void analyze(CompilationUnit compilationUnit) {
+        final String fullyQualifiedName = Analyzer.getFullyQualifiedName(compilationUnit);
+        compilationUnit.findAll(VariableDeclarationExpr.class).forEach((varDec) -> {
+            var parent = varDec.getParentNode().get();
+            if (parent instanceof ExpressionStmt) {
+                parent = parent.getParentNode().get();
+            }
+            final List<String> variablesNames = varDec.getVariables().stream()
+                    .map(VariableDeclarator::getNameAsString).collect(Collectors.toList());
+            final Set<String> usedNames = parent.findAll(NameExpr.class).stream()
+                    .map(NameExpr::getNameAsString).collect(Collectors.toSet());
+
+            if (!usedNames.containsAll(variablesNames)) {
+                addIssue(fullyQualifiedName, IssuesCodes.UNUSED_VARIABLE, new RawIssue(varDec));
+            }
+        });
+    }
+
+}
